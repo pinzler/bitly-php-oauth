5a9fb9ea8b83ea3e055ef338617521b1016d0cdc
pinzler <pinzler@gmail.com>
2013-05-02 14:25:11 -0300

    Update oauth.php

--- oauth.php
+++ oauth.php
@@ -7,10 +7,30 @@
 	$client_secret = "";
 	$redirect_uri = "http://.../oauth.php"; //The redirect_uri is just pointing back to the location of this file
 
-	$uri = file_get_contents("https://api-ssl.bitly.com/oauth/access_token?client_id=".$client_id."&client_secret=".$client_secret."&redirect_uri=".$redirect_uri."&code=".$code, 
-	    true);
+	$uri = "https://api-ssl.bitly.com/oauth/access_token"; //?client_id=".$client_id."&client_secret=".$client_secret."&redirect_uri=".$redirect_uri."&code=".$code;
+
+	//POST to the bitly authentication endpoint
+	$params = array();
+    	$params['client_id'] = $client_id;
+    	$params['client_secret'] = $client_secret;
+    	$params['code'] = $code;
+    	$params['redirect_uri'] = $redirect_uri;
 	
+	$output = "";
+  	$params_string = "";
+  	foreach ($params as $key=>$value) { $params_string .= $key.'='.$value.'&'; }
+  		rtrim($params_string,'&');
+  	try {
+		$ch = curl_init();
+		curl_setopt($ch,CURLOPT_URL, $uri);
+		curl_setopt($ch,CURLOPT_POST, count($params));
+		curl_setopt($ch,CURLOPT_POSTFIELDS, $params_string);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    $output = curl_exec($ch);
+  	} catch (Exception $e) {
+  	}
 	// Returns a URL encoded string in the format of "access_token=%s&login=%s&apiKey=%s"
+	
 	// parse_str() creates variables $access_token, $login, $apiKey (http://php.net/manual/en/function.parse-str.php)
 	parse_str($uri);
 
